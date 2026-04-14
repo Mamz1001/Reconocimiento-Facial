917c325cb40a95c13929732c5a20c392c89a3c72
Mamz1001 <miguel.antonio.martinez.zrt@gmail.com>
2026-04-13 19:51:40 -0600

    Eliminando basura

--- problema5.py
+++
@@ -1,6 +0,0 @@
-predicción = 0.8
-valor_real = 0
-def error(predicción, valor_real):
-    formula = (predicción - valor_real)**2
-    print(formula)
-error(predicción, valor_real)
--- problema6.py
+++
@@ -1,4 +0,0 @@
-peso = 0.8 #Aleatorio
-tasa = 0.1 #Tasa de aprendizaje
-gradiente = 0.4 #Calculado por backprop
-peso_nuevo = peso - (tasa*gradiente)
